70ab28d4bc5bf67143b05b8580d35ff4d77ffc46
junaidkamal <junaidkamal83@gmail.com>
2024-09-22 17:23:02 +0500

    Minor Changes

--- web.js
+++ web.js
@@ -14,12 +14,12 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
-var  typingEffects = new Typed(".typedText", {
-    strings:["Developer", "Amazon VA"],
+var typingEffects = new Typed(".typedText", {
+    strings: ["Developer", "Amazon VA"],
     loop: true,
-typeSpeed: 100,
-backSpeed: 80,
-backDelay:2000
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
 });
 
 
@@ -29,10 +29,10 @@ backDelay:2000
 let darkmode = document.querySelector('#darkmode');
 
 darkmode.onclick = () => {
-    if(darkmode.classList.contains('bx-moon')){
+    if (darkmode.classList.contains('bx-moon')) {
         darkmode.classList.replace('bx-moon', 'bx-sun');
         document.body.classList.add('active');
-    } else{
+    } else {
         darkmode.classList.replace('bx-sun', 'bx-moon');
         document.body.classList.remove('active');
     }
@@ -44,33 +44,32 @@ const sr = ScrollReveal({
     distance: "80px",
     duration: 2000,
     reset: true,
-  });
-  
-  sr.reveal(".social_icons", {});
-  sr.reveal(".featured-name", { delay: 200 });
-  sr.reveal(".hello", { delay: 200 });
-  sr.reveal(".featured-text-info", { delay: 200 });
-  sr.reveal(".featured-image", { delay: 200 });
-  sr.reveal(".heading", { delay: 200 });
-  sr.reveal(".services-content", { interval: 200 });
-  
-  const srLeft = ScrollReveal({
+});
+
+sr.reveal(".social_icons", {});
+sr.reveal(".featured-name", { delay: 200 });
+sr.reveal(".hello", { delay: 200 });
+sr.reveal(".featured-text-info", { delay: 200 });
+sr.reveal(".featured-image", { delay: 200 });
+sr.reveal(".heading", { delay: 200 });
+sr.reveal(".services-content", { interval: 200 });
+
+const srLeft = ScrollReveal({
     origin: "left",
     distance: "80px",
     duration: 2000,
     reset: true,
-  });
-  
-  srLeft.reveal(".about-container", { delay: 200 });
-  srLeft.reveal(".contact-form", { delay: 100 });
-  
-  const srRight = ScrollReveal({
+});
+
+srLeft.reveal(".about-container", { delay: 200 });
+srLeft.reveal(".contact-form", { delay: 100 });
+
+const srRight = ScrollReveal({
     origin: "right",
     distance: "80px",
     duration: 2000,
     reset: true,
-  });
-  
-  srRight.reveal(".portfolio-content", { delay: 200 });
-  srRight.reveal(".skill", { delay: 200 });
-  
+});
+
+srRight.reveal(".portfolio-content", { delay: 200 });
+srRight.reveal(".skill", { delay: 200 });
